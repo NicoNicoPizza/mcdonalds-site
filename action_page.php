fee202657f22cdf8cb1f71aeca5277212e50587f
NicoNicoPizza <47262000+NicoNicoPizza@users.noreply.github.com>
2021-01-11 23:38:42 +0100

    Update and rename action_page.html to action_page.php

---
+++ action_page.php
@@ -0,0 +1,12 @@
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>McDonalds</title>
+    </head>
+    <body>
+        <h1>Se ha iniciado sesión correctamente</h1>
+    </body>
+    <?php
+        echo "Se ha iniciado sesión correctamente"
+    ?>
+</html>
